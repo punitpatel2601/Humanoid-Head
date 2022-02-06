6cce2586d7e4906eb280f099d978a8e0350b4157
punitpatel2601 <punit.patel1@ucalgary.ca>
2022-02-06 00:49:18 -0700

    Two Servo Test (Arduino)

--- arduino_sketch/Arduino/humanoid_head_sketch/humanoid_head_sketch.ino
+++ arduino_sketch/Arduino/humanoid_head_sketch/humanoid_head_sketch.ino
@@ -1,14 +1,11 @@
 #include <ros.h>
-#include <ArduinoHardware.h>
+#include <Servo.h>
+
 #include <std_msgs/String.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/UInt16.h>
-#include <Servo.h>
-
 
 //********************** TESTING IN PROGRESS
-
-
 
 // Setting the pins on board for
 
@@ -17,16 +14,16 @@
 // #define LIGHT_SENSOR <PIN_NUMBER>
 
 // Outputs (LEDs, Motors, Sounds, etc.)
-#define GAS_WARN_LED 13
-#define SERVO_MOTOR 9
-
+#define GAS_WARN_LED 13 // Attach Gas Warning Light to Pin 13
+#define RIGHT_EAR 5     // Attach Right ear Servo to pin 5
+#define LEFT_EAR 6      // Attach Left ear Servo to pin 6
 
 // global variables
 ros::NodeHandle nh;
 std_msgs::String gas_reading;
 
-Servo servo;
-
+Servo rServo;
+Servo lServo;
 
 // Toggle LED light when the gas is detected
 void toggle_warning_led(const std_msgs::Bool led_toggle)
@@ -38,10 +35,15 @@ void toggle_warning_led(const std_msgs::Bool led_toggle)
 }
 
 // Updates the servo angle
-void change_ear_angle(const std_msgs::UInt16 &servo_command){
-  servo.write(servo_command.data); //set the servo angle between 0 to 180
+void update_rServo(const std_msgs::UInt16 &angle)
+{
+  rServo.write(angle.data);
 }
 
+void update_lServo(const std_msgs::UInt16 &angle)
+{
+  lServo.write(angle.data);
+}
 
 // List of publishers and Subscribers to ROS
 ros::Publisher gas_Sensor_Reading("Gas_Sensor", &gas_reading);
@@ -52,9 +54,8 @@ ros::Publisher light_Sensor_Reading("Light_Sensor", &light_reading);
 ros::Subscriber<std_msgs::Int64t> change_Light("Update_Light:, &update_light_intensity);
 */
 
-ros::Subscriber<std_msgs::UInt16> servo_Updates("Ear_Controller", change_ear_angle);
-
-
+ros::Subscriber<std_msgs::UInt16> right_servo("Right_Ear_Controller", update_rServo);
+ros::Subscriber<std_msgs::UInt16> left_servo("Left_Ear_Controller", update_lServo);
 
 void setup()
 {
@@ -68,16 +69,18 @@ void setup()
   nh.subscribe(warning_Led);
 
   // Ear Positioning
-  nh.subscribe(servo_Updates);
-  servo.attach(SERVO_MOTOR);
-}
+  rServo.attach(RIGHT_EAR);
+  lServo.attach(LEFT_EAR);
 
+  nh.subscribe(right_servo);
+  nh.subscribe(left_servo);
+}
 
 void loop()
 {
   std_msgs::String gas_reads;
   // gas_reads = <data from sensor at whatever pin it is>
-  
+
   gas_reads.data = "1500 3500";
   gas_Sensor_Reading.publish(&gas_reads);
 
--- arduino_sketch/Arduino/servo_test/servo_test.ino
+++ arduino_sketch/Arduino/servo_test/servo_test.ino
@@ -2,38 +2,40 @@
 #include <std_msgs/UInt16.h>
 #include <Servo.h>
 
-
 #define RIGHT_EAR 5 //Attach Right ear Servo to pin 5
 #define LEFT_EAR 6  //Attach Left ear Servo to pin 6
 
-
 ros::NodeHandle nh;
 
-Servo rServo; Servo lServo;
+Servo rServo;
+Servo lServo;
 
-void update_rServo(const std_msgs::UInt16 & angle) {
+void update_rServo(const std_msgs::UInt16 &angle)
+{
   rServo.write(angle.data);
 }
 
-void update_lServo(const std_msgs::UInt16 & angle) {
+void update_lServo(const std_msgs::UInt16 &angle)
+{
   lServo.write(angle.data);
 }
 
-ros::Subscriber<std_msgs::UInt16>right_servo("Right_Ear_Controller", update_rServo);
-ros::Subscriber<std_msgs::UInt16>left_servo("Left_Ear_Controller", update_lServo);
+ros::Subscriber<std_msgs::UInt16> right_servo("Right_Ear_Controller", update_rServo);
+ros::Subscriber<std_msgs::UInt16> left_servo("Left_Ear_Controller", update_lServo);
 
+void setup()
+{
+  nh.initNode();
 
-void setup() {
   rServo.attach(RIGHT_EAR);
   lServo.attach(LEFT_EAR);
-
-  nh.initNode();
 
   nh.subscribe(right_servo);
   nh.subscribe(left_servo);
 }
 
-void loop() {
+void loop()
+{
   nh.spinOnce();
   delay(2000);
 }
